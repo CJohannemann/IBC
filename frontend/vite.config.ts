d1684914f92e1aa43cdfc4bebae52a832579679c
Chris Johannemann <cjjohannemann@gmail.com>
2026-07-20 13:17:38 -0400

    gh pages?

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,22 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import { fileURLToPath, URL } from 'node:url'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/IBC/',
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [vue],
   base: '/IBC/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
 })
